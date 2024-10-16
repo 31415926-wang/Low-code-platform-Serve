08e72ed8683f02b5aae8d17ff32c58a2801cfbdb
wain <1550642660@qq.com>
2024-10-16 09:54:04 +0800

    10.16

--- app.js
+++ app.js
@@ -32,32 +32,32 @@ app.use(cors({
 
 /* 上传图片的接口 */
 // 使用 express-fileupload 中间件来处理文件上传
-app.use(fileUpload());
-app.post('/upload', (req, res) => {
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-    }
-    // 获取上传的文件，目前只处理单个
-    const uploadedFile = req.files['file-upload'];
-    console.log("后台监测上传图片", uploadedFile);
-    // console.log("后台监测上传图片2", req.files['file-img'].mv);
+// app.use(fileUpload());
+// app.post('/upload', (req, res) => {
+//     if (!req.files || Object.keys(req.files).length === 0) {
+//         return res.status(400).send('No files were uploaded.');
+//     }
+//     // 获取上传的文件，目前只处理单个
+//     const uploadedFile = req.files['file-upload'];
+//     console.log("后台监测上传图片", uploadedFile);
+//     // console.log("后台监测上传图片2", req.files['file-img'].mv);
 
-    // 生成文件名
-    const fileName = `${Date.now()}_${uploadedFile.name}`;
-    // 保存文件到指定目录
-    uploadedFile.mv(path.join(__dirname, 'uploads', fileName), (err) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        // 返回上传成功的图片路径
-        let result = {
-            data: `${fileName}`,
-            state: 'success',
-            code: 200
-        }
-        res.send(result);
-    });
-});
+//     // 生成文件名
+//     const fileName = `${Date.now()}_${uploadedFile.name}`;
+//     // 保存文件到指定目录
+//     uploadedFile.mv(path.join(__dirname, 'uploads', fileName), (err) => {
+//         if (err) {
+//             return res.status(500).send(err);
+//         }
+//         // 返回上传成功的图片路径
+//         let result = {
+//             data: `${fileName}`,
+//             state: 'success',
+//             code: 200
+//         }
+//         res.send(result);
+//     });
+// });
 
 // 设置读取图片文件目录
 app.use('/qsImgs', express.static(path.join(__dirname, 'uploads')));
@@ -76,8 +76,8 @@ app.post('/api/users/genVeriCode', function (req, res) {
 
 
 //指定静态资源的访问
-// app.use(express.static(path.resolve(__dirname, 'dist')))
-app.use(express.static(path.resolve(__dirname, 'docs')))
+app.use(express.static(path.resolve(__dirname, 'dist')))
+// app.use(express.static(path.resolve(__dirname, 'docs')))
 
 
 //自建 CORS 代理
